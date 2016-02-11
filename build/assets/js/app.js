e5ad2b4ed21edb580eacbeb50c726cfe572925ac
Max Tsvetkov <maxtsvetapple@gmail.com>
2016-02-11 10:25:22 +0200

    Added ru-loc for calendar

--- build/assets/js/app.js
+++ build/assets/js/app.js
@@ -4,6 +4,16 @@ $( document ).ready(function() {
 	  interval: 10000
 	});
 
+
+    $.datepicker.regional['ru'] = {
+		monthNames: ['Январь', 'Февраль', 'Март', 'Апрель',
+		'Май', 'Июнь', 'Июль', 'Август', 'Сентябрь',
+		'Октябрь', 'Ноябрь', 'Декабрь'],
+		dayNamesMin: ['Вс','Пн','Вт','Ср','Чт','Пт','Сб'],
+		firstDay: 1,
+	};
+	$.datepicker.setDefaults($.datepicker.regional['ru']);
+	
 	$( "#datepicker" ).datepicker();
 
 });
